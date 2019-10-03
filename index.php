6b2a24fe1ab6e194bca2140bad6717535718efaf
Uecki <51044891+Uecki@users.noreply.github.com>
2019-10-03 10:18:35 +0200

    Einführen der Filterfunktion für die Heatmap
    Eine Auswahl der Daten und ein zurücksetzen der Anzeige ist jetzt wie bei dem Activity Chart möglich.
    Außerdem heißt der Heatmap-Tab jetzt nicht  mehr "not finished" und die Filter-Felder bei dem Barchart wurden vorerst auskmmentiert.

--- index.php
+++ index.php
@@ -257,12 +257,17 @@ global $CFG;
 		    echo "var js_activity = ". $js_activity . ";\n";
 		    $js_barchart = json_encode($barchart, JSON_NUMERIC_CHECK);
 		    echo "var js_barchart = ". $js_barchart . ";\n";
+			$js_heatmap = json_encode($heatmap, JSON_NUMERIC_CHECK);
+		    echo "var js_heatmap = Object.entries(". $js_heatmap . ");\n";
 		?>
+		
+		
 		function toTimestamp(strDate){
 			var datum = Date.parse(strDate);
 			return datum/1000;
 		}
 		
+			//Filter for LineChart
         $(document).ready(function() {
             $('#dp_button_2').click(function() {
                 var start = document.getElementById('datepicker_3').value;
@@ -319,6 +324,513 @@ global $CFG;
 				
             });
         });
+		
+			//Filter for Heatmap
+		$(document).ready(function() {
+            $('#dp_button_3').click(function() {
+				
+                var start = document.getElementById('datepicker_5').value;
+        		var end = document.getElementById('datepicker_6').value;
+                /* rewrite date */
+                var s = start.split('.');
+                start = s[1]+'/'+s[0]+'/'+s[2];
+                /* rewrite date */
+                var e = end.split('.');
+                end = e[1]+'/'+e[0]+'/'+e[2];
+				start += ' 00:00:00';
+				end += ' 23:59:59';
+				var tp_start = toTimestamp(start);
+				var tp_end = toTimestamp(end);
+                if (tp_start <= tp_end){
+					
+						//Create heatmap data
+					var timespan;
+					//var heatmap_data_filtered = "[";
+					var heatmap_data_filtered = [];
+					var counterWeekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+					
+						//Associative array (object) for total number of weekday actions
+					var totalHits = {
+						"Monday"  : 0,
+						"Tuesday"  : 0,
+						"Wednesday"  : 0,
+						"Thursday"  : 0,
+						"Friday"  : 0,
+						"Saturday"  : 0,
+						"Sunday"  : 0
+					};
+					
+						//Associative array (object) for total  number of own weekday actions
+					var totalOwnHits = {
+						"Monday"  : 0,
+						"Tuesday"  : 0,
+						"Wednesday"  : 0,
+						"Thursday"  : 0,
+						"Friday"  : 0,
+						"Saturday"  : 0,
+						"Sunday"  : 0
+					};
+					
+					//Associative array (object) to assign the query results
+					var weekdays = {
+						"Monday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 0,
+						}, 
+						"Tuesday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 1,
+						}, 
+						"Wednesday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 2,
+						}, 
+						"Thursday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 3,
+						}, 
+						"Friday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 4,
+						}, 
+						"Saturday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 5,
+						}, 
+						"Sunday" : {
+							"0to6" : {
+								"all" : {
+									"col"  : 0,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 1,
+									"value" : 0,
+								},
+							},
+							"6to12" : {
+								"all" : {
+									"col"  : 2,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 3,
+									"value" : 0,
+								},
+							},
+								
+							"12to18" : {
+								"all" : {
+									"col"  : 4,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 5,
+									"value" : 0,
+								},
+							},
+								
+							"18to24" : {
+								"all" : {
+									"col"  : 6,
+									"value" : 0,
+								},
+								"own" : {
+									"col"  : 7,
+									"value" : 0,
+								},
+							},
+							"row" : 6,
+						}, 
+					};
+					
+						//Iterate through each element of the original query.
+					js_heatmap.forEach(function(item) {
+						
+							//Check, if the timestamp is included in the filter.
+						if (item[1].timecreated >= tp_start && item[1].timecreated <= tp_end) {
+						
+								//link timespan to column in heatmap
+							if(parseInt(item[1].hour) >= 0  && parseInt(item[1].hour) < 6) {
+								timespan = "0to6";		
+							}
+							else if(parseInt(item[1].hour) >= 6  && parseInt(item[1].hour) < 12) {
+								timespan = "6to12";			
+							}
+							else if(parseInt(item[1].hour) >= 12  && parseInt(item[1].hour) < 18) {
+								timespan = "12to18";				
+							}
+							else if(parseInt(item[1].hour) >= 18  && parseInt(item[1].hour) < 24) {
+								timespan = "18to24";			
+							}
+							
+								//Data for specific day
+							weekdays[item[1].weekday][timespan]["all"]["value"] += parseInt(item[1].allhits);
+							weekdays[item[1].weekday][timespan]["own"]["value"] += parseInt(item[1].ownhits);
+							
+								//Data for overall clicks
+							totalHits[item[1].weekday] += parseInt(item[1].allhits);
+							totalOwnHits[item[1].weekday] += parseInt(item[1].ownhits);
+
+
+						}
+					});
+					
+						//Put data of each weekdayfield into suitable format for the chart.
+					var counter = 0;
+					while (counter <= 6) {
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['0to6']['all']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['0to6']['all']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['0to6']['own']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['0to6']['own']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['6to12']['all']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['6to12']['all']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['6to12']['own']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['6to12']['own']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['12to18']['all']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['12to18']['all']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['12to18']['own']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['12to18']['own']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['18to24']['all']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['18to24']['all']['value']]);
+						
+						heatmap_data_filtered.push([weekdays[counterWeekday[counter]]['18to24']['own']['col'],weekdays[counterWeekday[counter]]['row'],weekdays[counterWeekday[counter]]['18to24']['own']['value']]);
+						
+						counter = counter + 1;
+					}
+					
+						//Put data of overall clicks into suitable format for the chart.
+					var x = 8; //for total and average hits
+					while(x <= 11) {
+						var y = 0; //for weekdays
+						while(y <= 6) {
+							if (x == 8) {
+								heatmap_data_filtered.push([x, y, totalHits[counterWeekday[y]]]);
+							}
+							else if (x == 9) {
+								heatmap_data_filtered.push([x, y, totalOwnHits[counterWeekday[y]]]);
+							}
+							else if (x == 10) {
+								heatmap_data_filtered.push([x, y, Math.round(totalHits[counterWeekday[y]]/7.0)]);
+							}
+							else if (x == 11) {
+								heatmap_data_filtered.push([x, y, Math.round(totalOwnHits[counterWeekday[y]]/7.0)]);
+							}
+							
+							y  = y+1;
+						}
+						x = x+1;
+					}
+					
+					
+                    Highcharts.chart('heatmap', {
+
+						chart: {
+							type: 'heatmap',
+							marginTop: 40,
+							marginBottom: 80,
+							plotBorderWidth: 1
+						},
+
+
+						title: {
+							text: 'Aktionen pro Tag pro Zeitraum'
+						},
+
+						xAxis: {
+							categories: ['ALLE<br>00:00-06:00', 'EIGENE<br>00:00-06:00', 'ALLE<br>06:00-12:00', 'EIGENE<br>06:00-12:00', 'ALLE<br>12:00-18:00', 'EIGENE<br>12:00-18:00','ALLE<br>18:00-24:00', 'EIGENE<br>18:00-24:00',  'ALLE<br>Gesamt', 'EIGENE<br>Gesamt', 'ALLE<br>Durchschnitt', 'EIGENE<br>Durchschnitt']
+						},
+
+						yAxis: {
+							categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+							title: null
+						},
+
+						colorAxis: {
+							min: 0,
+							minColor: '#FFFFFF',
+							maxColor: Highcharts.getOptions().colors[0]
+						},
+
+						legend: {
+							align: 'right',
+							layout: 'vertical',
+							margin: 0,
+							verticalAlign: 'top',
+							y: 25,
+							symbolHeight: 280
+						},
+						
+						tooltip: false,
+						
+
+						series: [{
+							name: 'Actions per day',
+							borderWidth: 1,
+							data: heatmap_data_filtered, //convert data string to array
+							dataLabels: {
+								enabled: true,
+								color: '#000000'
+							}
+						}]
+
+					}); 
+                }else{
+                    // Materialize.toast(message, displayLength, className, completeCallback);
+                    Materialize.toast('Überprüfen Sie ihre Auswahl (Beginn < Ende)', 3000) // 4000 is the duration of the toast
+                    $('#datepicker_5').val("");
+                    $('#datepicker_6').val("");
+                }
+				
+            });
+        });
+		
     </script>
 	
     <!-- Reset Charts (BarChart, LineChart) -->
@@ -329,6 +841,8 @@ global $CFG;
         $('#rst_btn_1').click(function() {
         /* do something */
         });
+		
+			//Line Chart - reset button
         $('#rst_btn_2').click(function() {
             var data = new google.visualization.DataTable();
 				data.addColumn('date', 'Datum');
@@ -351,6 +865,14 @@ global $CFG;
 			
 			//var test = "<?php echo $allData ?>";
 			//console.log(test);
+        });
+		
+		//Heatmap - reset button
+        $('#rst_btn_3').click(function() {
+			drawHeatMap();
+			$('#datepicker_3').val("");
+            $('#datepicker_4').val("");
+			
         });
 
 
@@ -453,7 +975,7 @@ global $CFG;
                         <a href="#chart2">Activity Chart</a>
                     </li>
                     <li class="tab" id="tab_heatMap">
-                        <a href="#chart3">not finished (heat map)</a>
+                        <a href="#chart3">Heatmap</a>
                     </li>
                     <li class="tab" id="tab_treeMap">
                         <a href="#chart4">Treemap</a>
@@ -468,6 +990,15 @@ global $CFG;
                     <div id="options" class="col s3">
                         <div class="row">
                             <div class="input-field col s12">
+							<!--
+								<div class="divider"></div>
+								<p>Filter:</p>
+                                    <input placeholder="Beginn" type="text" class="datepick " id="datepicker_1">
+                                    <input placeholder="Ende" type="text" class="datepick " id="datepicker_2">
+                                    <button class="btn waves-effect waves-light grey darken-3 button" type="submit" name="action" id="dp_button_1">Aktualisieren</button>
+                                    <button class="btn waves-effect waves-light grey darken-3 button" type="submit" name="action" id="rst_btn_1">R&uuml;ckg&auml;ngig</button>
+                                    <div class="divider"></div>
+								-->
                                 <p>Datensicherung:</p>
 								
                                 <!-- Button nicht mehr nötig.
@@ -536,10 +1067,13 @@ global $CFG;
                         <div id="options" class="col s3">
                             <div class="row">
                                 <div class="input-field col s12">
-									<!--
-                                    <button class="btn waves-effect waves-light grey button" type="submit" name="action" id="rst_btn_3">R&uuml;ckg&auml;ngig</button>
+									<div class="divider"></div>
+									<p>Filter:</p>
+                                    <input placeholder="Beginn" type="text" class="datepick " id="datepicker_5">
+                                    <input placeholder="Ende" type="text" class="datepick " id="datepicker_6">
+                                    <button class="btn waves-effect waves-light grey darken-3 button" type="submit" name="action" id="dp_button_3">Aktualisieren</button>
+                                    <button class="btn waves-effect waves-light grey darken-3 button" type="submit" name="action" id="rst_btn_3">R&uuml;ckg&auml;ngig</button>
                                     <div class="divider"></div>
-									-->
                                     <p>Datensicherung:</p>
 									
 									<form action='create_html.php' method='post' id='download_form_2'>
