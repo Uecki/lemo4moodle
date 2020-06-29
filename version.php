a3aaceab7e3690026a5443d91af0b7597ade29cd
Uecki <51044891+Uecki@users.noreply.github.com>
2020-06-29 21:54:00 +0200

    Updated version file.

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_lemo4moodle';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494).
-$plugin->version = 2020032000;  // YYYYMMDDHH (Releasedate of plugin).
+$plugin->version = 2020062900;  // YYYYMMDDHH (Releasedate of plugin).
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0).
